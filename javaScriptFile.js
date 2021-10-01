c4636490590d004d2c7bcf1f0603c27614f429d1
rawan411 <rawanwaleed613@gmail.com>
2021-10-02 00:01:18 +0200

    first commit

---
+++ javaScriptFile.js
@@ -0,0 +1,10 @@
+
+         
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#check').addEventListener('click',function(){
+        let firstName=document.getElementById('first Name').value;
+            let secondName=document.getElementById('Second Name').value;
+            alert("Welcome " + firstName + " " + secondName);
+            });
+     });
+        
